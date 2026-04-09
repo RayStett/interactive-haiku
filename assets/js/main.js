71e56ddbc7e15d4594e2e40b2f17591d72be9af9
RayStett <rpstettler@outlook.com>
2026-04-09 05:38:32 +0000

    wahhhh

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,15 +11,15 @@ document.addEventListener("click", function () {
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + 1) + min; 
 }
-let elements = document.querySelectorAll("body *"); 
-console.log(elements.length); 
+//let elements = document.querySelectorAll("body *"); 
+//console.log(elements.length); 
 let divs = document.querySelection(".grid-container div"); 
 
 function randomize() {
     divs.forEach(function(div) {
         let scale = randomNumber(.5, 1.2);
         let translateX = randomNumber(0, 50);
-        let translateY = randomNUmber(0, 360);
+        let translateY = randomNumber(0, 360);
         let rotate = randomNumber(0, 360);
         div.style.transform = 
             `scale(${scale})
@@ -29,7 +29,7 @@ function randomize() {
 
     console.log(divs.length, "randomize! "); 
 }
-
+/*
 elements.forEach(function(ele) {
 	ele.style.transform = `rotate(${Math.random() * 360}deg)`;
-});
+}); */ 
