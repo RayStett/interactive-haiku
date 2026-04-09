f6d1a63ffe6d8f4687133f4780b72334ca17cf1c
RayStett <rpstettler@outlook.com>
2026-04-09 05:14:32 +0000

    i think she's broken

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,25 @@ document.addEventListener("click", function () {
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + 1) + min; 
 }
+let elements = document.querySelectorAll("body *"); 
+console.log(elements.length); 
+let divs = document.querySelection(".grid-container div"); 
+
+function randomize() {
+    divs.forEach(function(div) {
+        let scale = randomNumber(.5, 1.2);
+        let translateX = randomNumber(0, 50);
+        let translateY = randomNUmber(0, 360);
+        let rotate = randomNumber(0, 360);
+        div.style.transform = 
+            `scale(${scale})
+            translate(${translateX}%, ${translateY}%)
+            rotate(${rotate}deg)`; 
+    })
+
+    console.log(divs.length, "randomize! "); 
+}
+
+elements.forEach(function(ele) {
+	ele.style.transform = `rotate(${Math.random() * 360}deg)`;
+});
